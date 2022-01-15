5279af5a81a34201fde5ea98888b1df938a13fc8
Flying Saucer <skdigitaleyes@gmail.com>
2022-01-15 11:39:26 +0000

    test: Add test of user minting NFTs

--- __test__/nft.ava.ts
+++ __test__/nft.ava.ts
@@ -13,15 +13,35 @@ import {
 const base_cost = NEAR.parse("1 N");
 const min_cost = NEAR.parse("0.01 N");
 
+const createNewAccout = async (
+  root: NearAccount,
+  accountName: string,
+  initialBalance: string = "15 N"
+) => {
+  const account = await root.createAccount(accountName, {
+    initialBalance: NEAR.parse(initialBalance).toString(),
+  });
+  const accountAddress = account.accountId;
+  const accountBalance = await account.balance();
+  const accountBalanceHuman = accountBalance.available.toHuman();
+  console.log(`${accountAddress} created with balance ${accountBalanceHuman}`);
+  return account;
+};
+
 const runner = Workspace.init(
   { initialBalance: NEAR.parse("15 N").toString() },
   async ({ root }) => {
+    console.log(`Is testnet? ${Workspace.networkIsTestnet()}`);
     // root will be test.near
-
+    const rootBalance = await root.balance();
+    const rootBalanceHuman = rootBalance.available.toHuman();
+    console.log({ rootBalanceHuman });
+    const alice = await createNewAccout(root, "alice", "50 N");
+    const bob = await createNewAccout(root, "bob", "15 N");
     // the contract will be deployed on tenk.test.near
     // and new_default_meta function is being called to initialize it
     const tenk = await deploy(root, "tenk", { base_cost, min_cost });
-    return { tenk };
+    return { tenk, alice, bob };
   }
 );
 
@@ -36,6 +56,12 @@ runner.test("can get cost per token", async (t, { tenk }) => {
       " to store"
   );
 
+  t.log(
+    `Const per token for 24 is: ${await (
+      await costPerToken(tenk, 24)
+    ).toHuman()}`
+  );
+
   t.deepEqual(cost.toBigInt(), base_cost.add(storageCost).toBigInt());
   if (cost.toBigInt() > 0) {
     t.assert(cost.gt(await costPerToken(tenk, 24)));
@@ -45,8 +71,11 @@ runner.test("can get cost per token", async (t, { tenk }) => {
 async function assertXTokens(t, root: NearAccount, tenk, num) {
   const method = num == 1 ? "nft_mint_one" : "nft_mint_many";
   let args = num == 1 ? {} : { num };
+  const cost = await totalCost(tenk, num);
+
+  t.log(`${num} token costs ` + cost.toHuman());
   const res = await root.call_raw(tenk, method, args, {
-    attachedDeposit: await totalCost(tenk, num),
+    attachedDeposit: cost,
     gas: MINT_ONE_GAS,
   });
   t.true(res.succeeded, [res.Failure, ...res.promiseErrorMessages].join("\n"));
@@ -65,5 +94,38 @@ async function assertXTokens(t, root: NearAccount, tenk, num) {
 ].forEach(async ([num, x]) => {
   runner.test("mint " + num, async (t, { root, tenk }) => {
     await assertXTokens(t, root, tenk, x);
+  });
+});
+
+async function userMintsNFTs(t, user: NearAccount, tenk, num) {
+  const method = num == 1 ? "nft_mint_one" : "nft_mint_many";
+  let args = num == 1 ? {} : { num };
+  const cost = await totalCost(tenk, num);
+
+  t.log(
+    `${user.accountId} is minting: ${num} tokens costing ` + cost.toHuman()
+  );
+  const userBalanceBefore = (await user.balance()).available.toHuman();
+  t.log(`Balance Before: ${userBalanceBefore}`);
+  const res = await user.call_raw(tenk, method, args, {
+    attachedDeposit: cost,
+    gas: MINT_ONE_GAS,
+  });
+  t.true(res.succeeded, [res.Failure, ...res.promiseErrorMessages].join("\n"));
+  const userBalanceAfter = (await user.balance()).available.toHuman();
+  t.log(`Balance After: ${userBalanceAfter}`);
+  t.is(num, (await nftTokensForOwner(user, tenk)).length);
+  if (num == 30 && Workspace.networkIsTestnet()) {
+    await deployEmpty(tenk);
+  }
+}
+
+[
+  ["one", 1],
+  ["two", 2],
+  ["ten", 10],
+].forEach(async ([num, x]) => {
+  runner.test("alice mints " + num, async (t, { alice, tenk }) => {
+    await userMintsNFTs(t, alice, tenk, x);
   });
 });
--- commands.md
+++ commands.md
@@ -8,6 +8,12 @@ yarn test --help # To get help
 yarn test __test__/nft.ava.ts # Runnings tests in specific file
 yarn test __test__/nft.ava.ts -c 1 -v # concurrency is 1 and verbose mode
 
+# Running tests related to NFT mints
+yarn test __test__/nft.ava.ts -c 1 -v
+# Running tests related to Paras
+yarn test __test__/paras.ava.ts -c 1 -v
+
+
 # RPC Calls
 
 near deploy --wasmFile target/wasm32-unknown-unknown/release/tenk.wasm --accountId flyingsaucertenk.testnet

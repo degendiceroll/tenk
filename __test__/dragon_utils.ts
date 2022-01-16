1020e556a2809a39729e3ecd62dda84034200bc5
Flying Saucer <skdigitaleyes@gmail.com>
2022-01-16 12:17:28 +0000

    add dragon_utils file

---
+++ __test__/dragon_utils.ts
@@ -0,0 +1,24 @@
+import { NEAR } from "near-units";
+import { NearAccount } from "near-willem-workspaces-ava";
+
+export const createNewAccout = async (
+  root: NearAccount,
+  accountName: string,
+  initialBalance: string = "15 N"
+) => {
+  const account = await root.createAccount(accountName, {
+    initialBalance: NEAR.parse(initialBalance).toString(),
+  });
+  const accountAddress = account.accountId;
+  const accountBalance = await account.balance();
+  const accountBalanceHuman = accountBalance.available.toHuman();
+  console.log(`${accountAddress} created with balance ${accountBalanceHuman}`);
+  return account;
+};
+
+export const printBalance = async (t, user: NearAccount) => {
+  const userAddress = user.accountId;
+  const userBalance = await user.balance();
+  const userBalanceHuman = userBalance.available.toHuman();
+  t.log(`${userAddress} has ${userBalanceHuman}`);
+};
